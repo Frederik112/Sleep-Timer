db223b6c275cb4a59d1a7bf91354093640f3c12b
Frederik112 <Frederik112@hotmail.com>
2017-01-17 22:52:14 +0100

    Add files via upload

---
+++ SleepFromTimer/SleepFromTimer/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace SleepFromTimer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.startButton = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Timer = new System.Windows.Forms.Label();
+            this.Minutter = new System.Windows.Forms.Label();
+            this.countDownShow = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.startButton.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.ForeColor = System.Drawing.SystemColors.Control;
+            this.startButton.Location = new System.Drawing.Point(121, 127);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 2;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.button1_Click);
+            this.startButton.MouseEnter += new System.EventHandler(this.startButton_MouseEnter);
+            this.startButton.MouseLeave += new System.EventHandler(this.startButton_MouseLeave);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Timer
+            // 
+            this.Timer.AutoSize = true;
+            this.Timer.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Timer.ForeColor = System.Drawing.SystemColors.Control;
+            this.Timer.Location = new System.Drawing.Point(108, 57);
+            this.Timer.Name = "Timer";
+            this.Timer.Size = new System.Drawing.Size(45, 15);
+            this.Timer.TabIndex = 3;
+            this.Timer.Text = "Timer";
+            // 
+            // Minutter
+            // 
+            this.Minutter.AutoSize = true;
+            this.Minutter.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Minutter.ForeColor = System.Drawing.SystemColors.Control;
+            this.Minutter.Location = new System.Drawing.Point(159, 57);
+            this.Minutter.Name = "Minutter";
+            this.Minutter.Size = new System.Drawing.Size(62, 15);
+            this.Minutter.TabIndex = 4;
+            this.Minutter.Text = "Minutter";
+            // 
+            // countDownShow
+            // 
+            this.countDownShow.AutoSize = true;
+            this.countDownShow.Font = new System.Drawing.Font("Stencil", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.countDownShow.ForeColor = System.Drawing.SystemColors.Control;
+            this.countDownShow.Location = new System.Drawing.Point(12, 222);
+            this.countDownShow.Name = "countDownShow";
+            this.countDownShow.Size = new System.Drawing.Size(0, 19);
+            this.countDownShow.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(113, 75);
+            this.textBox1.MaxLength = 1;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(33, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(163, 75);
+            this.textBox2.MaxLength = 2;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(33, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(313, 311);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.countDownShow);
+            this.Controls.Add(this.Minutter);
+            this.Controls.Add(this.Timer);
+            this.Controls.Add(this.startButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Sleep Countdown";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label Timer;
+        private System.Windows.Forms.Label Minutter;
+        private System.Windows.Forms.Label countDownShow;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ SleepFromTimer/SleepFromTimer/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SleepFromTimer
+{
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int Countdown = 0;
+
+        private string udregnTidFraSekunder()
+        {
+            int tempCountDown = Countdown;
+            int hours = 0;
+            int minutes = 0;
+            int seconds = 0;
+
+            if(tempCountDown/3600 > 0)
+            {
+                hours = tempCountDown / 3600;
+                tempCountDown -= hours * 3600;
+            }
+            if(tempCountDown/60 > 0)
+            {
+                minutes = tempCountDown / 60;
+                tempCountDown -= minutes * 60;
+            }
+            seconds = tempCountDown;
+
+            string hoursText = hours.ToString();
+            string minutesText = minutes.ToString();
+            string secondsText = seconds.ToString();
+
+            if(hoursText.Length != 2)
+            {
+                hoursText = "0" + hoursText;
+            }
+            if(minutesText.Length != 2)
+            {
+                minutesText = "0" + minutesText;
+            }
+            if(secondsText.Length != 2)
+            {
+                secondsText = "0" + secondsText;
+            }
+
+            return hoursText + ":" + minutesText + ":" + secondsText;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(startButton.Text == "Stop")
+            {
+                timer1.Stop();
+                Countdown = 0;
+                countDownShow.Text = "";
+                startButton.Text = "Start";
+                textBox1.Text = "";
+                textBox2.Text = "";
+                return;
+            }
+
+            if((textBox1.Text == null || textBox1.Text == "") && (textBox2.Text == null || textBox2.Text == "") )
+            {
+                return;
+            }
+            
+            startButton.Text = "Stop";
+            int timerISekunder = 0;
+            int minutterISekunder = 0;
+            if (textBox1.Text != null && textBox1.Text != "")
+            {
+                timerISekunder = Convert.ToInt16(textBox1.Text) * 60 * 60;
+            }
+            if (textBox2.Text != null && textBox2.Text != "")
+            {
+                minutterISekunder = Convert.ToInt16(textBox2.Text) * 60;
+            }
+                
+            Countdown = timerISekunder + minutterISekunder;
+            countDownShow.Text = udregnTidFraSekunder();
+            timer1.Start();
+
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Countdown--;
+            countDownShow.Text = udregnTidFraSekunder();
+
+            if(Countdown == 0)
+            {
+                timer1.Stop();
+                startButton.Text = "Start";
+                Application.SetSuspendState(PowerState.Suspend, false, false);
+            }
+        }
+
+        
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+   
+            Timer.BackColor = Color.Transparent;
+            Minutter.BackColor = Color.Transparent;
+            countDownShow.BackColor = Color.Transparent;
+            textBox1.BorderStyle = BorderStyle.Fixed3D;
+            textBox2.BorderStyle = BorderStyle.Fixed3D;
+            startButton.BackColor = Color.Transparent;
+            startButton.TabStop = false;
+            startButton.TabStop = false;
+            startButton.FlatStyle = FlatStyle.Flat;
+            startButton.FlatAppearance.BorderSize = 0;
+            startButton.FlatAppearance.BorderColor = Color.FromArgb(0, 255, 255, 255);
+            startButton.FlatAppearance.CheckedBackColor = Color.White;
+            startButton.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            startButton.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            
+        }
+
+        private void startButton_MouseEnter(object sender, EventArgs e)
+        {
+            startButton.FlatAppearance.BorderSize = 1;
+            startButton.FlatAppearance.BorderColor = Color.White;
+        }
+
+        private void startButton_MouseLeave(object sender, EventArgs e)
+        {
+            startButton.FlatAppearance.BorderSize = 0;
+            startButton.FlatAppearance.BorderColor = Color.FromArgb(0, 255, 255, 255);
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+
+                MessageBox.Show("Kun tal");
+                e.Handled = true;
+            }
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+
+                MessageBox.Show("Kun tal");
+                e.Handled = true;
+            }
+        }
+
+        
+    }
+}
